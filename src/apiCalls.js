dcd7d1be39e23e8649c9310371a700b3b22e170d
Peyton Jones <peytonj.dev@gmail.com>
2021-01-19 11:24:06 -0700

    create fetch and post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,19 @@
-export const getOrders = () => {
+ const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-      .then(data => data)
+    .then(response => response.json())
+    .then(data => data)
 }
+
+ const postOrders = async (data = {}) => {
+  const response = await fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  return response.json();
+}
+
+export {getOrders, postOrders}
