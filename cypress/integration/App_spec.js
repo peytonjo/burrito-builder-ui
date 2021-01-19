e1155ac820c8bbadacfcdeaae4924b66975a3e45
Peyton Jones <peytonj.dev@gmail.com>
2021-01-19 11:51:41 -0700

    complete passing tests for application

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -3,9 +3,17 @@ describe('App', () => {
     cy.visit('http://localhost:3000/')
   })
 
+  it('should have a title on render', () => {
+    cy.get('h1').contains('Burrito Builder')
+  })
+
   it('Should have an input and submit button on render', () => {
     cy.get('input')
     cy.get(':nth-child(15)')
+  })
+
+  it('Should take a name into the input field', () => {
+    cy.get('input').type('Peyton')
   })
 
   it('should display all order card on render', () => {
@@ -29,8 +37,18 @@ describe('App', () => {
   it('Should alert the user when clicking the submit button with no selected ingredients', () => {
     cy.get('header')
     cy.get('form')
+    cy.get('button').contains('Submit Order').click()
     cy.on('window:alert',(txt)=>{
-      expect(txt).to.contains('You need to select at least one ingredient to place an order! ');
+      expect(txt).to.contains('You are missing an ingredient or name for the order :)');
+    })
+  })
+
+  it('Should alert the user if no name is entered', () => {
+    cy.get('header')
+    cy.get('form')
+    cy.get('button').contains('Submit Order').click()
+    cy.on('window:alert',(txt)=>{
+      expect(txt).to.contains('You are missing an ingredient or name for the order :)');
     })
   })
 
@@ -53,7 +71,7 @@ describe('App', () => {
     cy.get('button').contains('beans').click()
     cy.get('button').contains('hot sauce').click()
     cy.get('button').contains('sour cream').click()
-    cy.get(':nth-child(15)').contains('Submit Order').click()
+    cy.get('button').contains('Submit Order').click()
 
     cy.get('section').contains('Peyton')
     cy.get('section').contains('beans')
