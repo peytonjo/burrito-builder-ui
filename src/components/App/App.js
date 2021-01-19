e1b87f6cccd8dd2c9645021f4ddbd6394fb8651c
Peyton Jones <peytonj.dev@gmail.com>
2021-01-19 11:23:13 -0700

    pass props to orderform

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
-  constructor(props) {
+  constructor() {
     super();
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+      .then(orders => this.setState({orders: orders.orders}))
       .catch(err => console.error('Error fetching:', err));
+  }
+//function should change apps state 
+  addOrder = (order) => {
+    const oldOrders = [...this.state.orders]
+    oldOrders.push(order)
+    this.setState({orders: oldOrders})
   }
 
   render() {
@@ -19,7 +29,7 @@ class App extends Component {
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm addOrder={this.addOrder}/>
         </header>
 
         <Orders orders={this.state.orders}/>
